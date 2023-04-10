e4cca74d9e8c06d7369d490945ab37cc394b91b0
Luan Bianchini <luanbianchine1@gmail.com>
2023-04-10 18:05:52 -0300

    feat: add styles

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text } from 'react-native';
+import { styles } from '../styles/home.styled';
 
 export default function Home() {
-  return <Text>Home</Text>;
+  return (
+    <View style={styles.container}>
+      <Text>Home</Text>
+    </View>
+  );
 }
