699f8d5fc6f3ae884587d57de77ac0a1f434678e
Luan Bianchini <luanbianchine1@gmail.com>
2023-04-10 11:55:23 -0300

    feat: fist home page view

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Text } from 'react-native';
+import Home from './src/views/Home';
 
 export default function App() {
   return (
     <>
-      <Text>Hello World</Text>
+      <Home />
     </>
   );
 }
