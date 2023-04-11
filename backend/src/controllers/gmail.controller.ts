95be3ccbc010cc75909c963a7b832b2eecf45e41
Luan Bianchini <luanbianchine1@gmail.com>
2023-04-11 19:24:21 -0300

    feat: clean content to use in methods

--- backend/src/controllers/gmail.controller.ts
+++ backend/src/controllers/gmail.controller.ts
@@ -4,7 +4,13 @@ export class GmailController implements IGmailController {
   private content?: string[];
 
   async setContent(content: string[]): Promise<void> {
-    this.content = content;
+    const cleanContent: string[][] = [];
+    content.map((el) => {
+      const element = el.replace(/\n/g, ' ').split(' ');
+      cleanContent.push(element);
+    });
+    this.content = cleanContent.flat();
+    this.getValue();
   }
 
   async getValue() {
