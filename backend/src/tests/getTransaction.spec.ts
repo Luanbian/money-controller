cdb0042b6221f1ce6e2395a5e3320c73ddaa9605
Luan Bianchini <luanbianchine1@gmail.com>
2023-04-20 14:36:04 -0300

    feat: add tests do getTransaction

---
+++ backend/src/tests/getTransaction.spec.ts
@@ -0,0 +1,16 @@
+import { it, expect, vi, describe  } from 'vitest';
+import { GmailController } from '../controllers/gmail.controller';
+import { IGmailGateway } from '../interfaces/interfaces';
+
+describe('get Transaction datas', () => {
+    it('should be able to show bank, date and value of transaction', async () => {
+        const getTransactionMock = vi.fn().mockResolvedValue([
+            { bank: 'bank', date: 'date', value: 'value' }
+        ])
+        const gmailController = new GmailController({getTransaction: getTransactionMock} as IGmailGateway)
+        await expect(gmailController.getTransaction()).resolves.toEqual([
+            { bank: 'bank', date: 'date', value: 'value' }
+        ])
+        expect(getTransactionMock).toHaveBeenCalled();
+    })
+})
