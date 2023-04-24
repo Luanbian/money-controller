d52e3a2b8ae79fc0bb253351f26984e03edfcd19
Luan Bianchini <luanbianchine1@gmail.com>
2023-04-24 15:13:09 -0300

    feat: update expense

--- Mobile/src/components/todolist.tsx
+++ Mobile/src/components/todolist.tsx
@@ -21,6 +21,7 @@ export const Todolist = () => {
   const [inputExpense, setInputExpense] = useState('');
   const [inputValue, setInputValue] = useState('');
   const [listExpenses, setListExpenses] = useState<ListExpenses[]>([]);
+  const [selectedExpense, setSelectedExpense] = useState<number | null>(null);
 
   useEffect(() => {
     axios
@@ -44,34 +45,59 @@ export const Todolist = () => {
     setPopUp(false);
   };
 
+  const handleupdateObject = (id?: number | null) => {
+    if (inputExpense.length < 3) return;
+    const expense: IExpenseInput = { expense: inputExpense, value: Number(inputValue) };
+
+    axios.put(`${baseURL}/expense/${id}`, expense).then((response) => console.log(response.data));
+
+    setInputExpense('');
+    setInputValue('');
+    setPopUp(false);
+  };
+
   return (
     <View>
       {popUp && (
-        <View>
-          <TextInput value={inputExpense} onChangeText={setInputExpense} placeholder="Nome da despesa" />
-          <TextInputMask
-            value={inputValue}
-            onChangeText={setInputValue}
-            placeholder="Valor da despesa"
-            type={'money'}
-            options={{
-              precision: 2,
-              separator: '.',
-              delimiter: '.',
-              unit: '',
-              suffixUnit: '',
-            }}
-            keyboardType="numeric"
-          />
-          <Button title="Adicionar Despesa" onPress={handleAddObject} />
-        </View>
+        <>
+          <>
+            <TextInput value={inputExpense} onChangeText={setInputExpense} placeholder="Nome da despesa" />
+            <TextInputMask
+              value={inputValue}
+              onChangeText={setInputValue}
+              placeholder="Valor da despesa"
+              type={'money'}
+              options={{
+                precision: 2,
+                separator: '.',
+                delimiter: '.',
+                unit: '',
+                suffixUnit: '',
+              }}
+              keyboardType="numeric"
+            />
+          </>
+          {!selectedExpense ? (
+            <Button title="Adicionar Despesa" onPress={handleAddObject} />
+          ) : (
+            <Button title="Atualizar Despesa" onPress={() => handleupdateObject(selectedExpense)} />
+          )}
+        </>
       )}
-      {!popUp && <Button title="+" onPress={() => setPopUp(true)} />}
+      {!popUp && <Button title="add despesa" onPress={() => setPopUp(true)} />}
       {listExpenses &&
         listExpenses.map((object) => (
           <View key={object.id.toString()}>
             <Text>{object.expense}</Text>
             <Text>{object.value}</Text>
+            <Button
+              title="Atualizar Despesa"
+              onPress={() => {
+                setPopUp(true);
+                const id = object.id;
+                setSelectedExpense(id);
+              }}
+            />
           </View>
         ))}
     </View>
