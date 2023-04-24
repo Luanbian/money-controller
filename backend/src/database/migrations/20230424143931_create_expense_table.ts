c7d69cee5e0a0b955bd90f6e8c4d5a6f46be8db3
Luan Bianchini <luanbianchine1@gmail.com>
2023-04-24 11:48:46 -0300

    feat: created expense table in db

--- backend/src/database/migrations/20230424143931_create_expense_table.ts
+++ backend/src/database/migrations/20230424143931_create_expense_table.ts
@@ -1,10 +1,15 @@
 import { Knex } from "knex";
 
-
 export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable('expenses', table => {
+        table.increments('id'),
+        table.string('expense'),
+        table.float('value'),
+        table.boolean('isPaid').defaultTo(false)
+    });
 }
 
 
 export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTable('expenses');
 }
-
