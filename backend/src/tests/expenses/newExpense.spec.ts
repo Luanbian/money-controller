72e16ea2522ddd17358b3ce1340dd544f6a289eb
Luan Bianchini <luanbianchine1@gmail.com>
2023-04-25 18:51:00 -0300

    feat: add test to create expense

---
+++ backend/src/tests/expenses/newExpense.spec.ts
@@ -0,0 +1,20 @@
+import { describe, expect, it, vi } from "vitest";
+import { ExpenseController } from "../../controllers/expense.controller";
+
+describe('Create a new expense', () => {
+    it('should be able to create a new expense', async () => {
+        const expenseMock = vi.fn().mockResolvedValue([1]);
+        const expenseController = new ExpenseController({expense: expenseMock} as any)
+        const input = {
+            expense: 'Teste',
+            value: 1
+        };
+        await expect(expenseController.newExpense(input)).resolves.toEqual({
+            statusCode: 201,
+            message: 'created',
+            data: [ 1 ]
+        });
+        expect(expenseMock).toHaveBeenCalledOnce();
+        expect(expenseMock).toHaveBeenCalledWith(input);
+    })
+})
--- backend/src/tests/gmail/getTransaction.spec.ts
+++ backend/src/tests/gmail/getTransaction.spec.ts
@@ -1,6 +1,6 @@
 import { it, expect, vi, describe  } from 'vitest';
-import { GmailController } from '../controllers/gmail.controller';
-import { IGmailGateway } from '../interfaces/interfaces';
+import { GmailController } from '../../controllers/gmail.controller';
+import { IGmailGateway } from '../../interfaces/interfaces';
 
 describe('get Transaction datas', () => {
     it('should be able to show bank, date and value of transaction', async () => {
