192e8abd85703c24606f3891865fa51646fad6be
Luan Bianchini <luanbianchine1@gmail.com>
2023-04-27 12:30:45 -0300

    refactor: apply standard spaces in file

--- backend/src/helpers/helper.ts
+++ backend/src/helpers/helper.ts
@@ -1,12 +1,11 @@
 import { IHelper, IHttpsResponse } from "../interfaces/interfaces";
 
-
-export class Helper implements IHelper{
-    ok({ statusCode, data, message }: IHttpsResponse): IHttpsResponse {
-        return {
-          statusCode: statusCode ?? 200,
-          message: message ?? 'ok',
-          data,
-        }
-    }
+export class Helper implements IHelper {
+  ok({ statusCode, data, message }: IHttpsResponse): IHttpsResponse {
+    return {
+      statusCode: statusCode ?? 200,
+      message: message ?? "ok",
+      data,
+    };
+  }
 }
