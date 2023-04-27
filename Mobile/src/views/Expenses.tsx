eadf0ce518ff3bd86650a2aff4295cd02c478e1a
Luan Bianchini <luanbianchine1@gmail.com>
2023-04-27 12:33:13 -0300

    fix: name of file

--- Mobile/src/views/Expenses.tsx
+++ Mobile/src/views/Expenses.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
-import { ExpensesFixedList } from '../components/todolist';
+import { ExpensesFixedList } from '../components/ExpensesFixedList';
 import { styles } from '../styles/home.styled';
 
 export default function Expenses() {
