6cf60545dfb44d2c2f077448921de6bb2e92df8c
Luan Bianchini <luanbianchine1@gmail.com>
2023-05-03 14:56:47 -0300

    feat: apply httpResponse ion returns

--- backend/src/controllers/expense.controller.ts
+++ backend/src/controllers/expense.controller.ts
@@ -1,21 +1,19 @@
 import { IExpenseController, IExpenseDatabase, IHttpsResponse, IinputNewExpense } from '../interfaces/interfaces';
-
+import { HttpResponseHelper } from '../helpers/helper';
 export class ExpenseController implements IExpenseController {
-    constructor(
-        private readonly expenseDatabase: IExpenseDatabase, 
-    ) {}
+    constructor(private readonly expenseDatabase: IExpenseDatabase) {}
 
     async newExpense(expense: IinputNewExpense): Promise<IHttpsResponse> {
         const expenses = await this.expenseDatabase.newExpense({
             expense: expense.expense,
             value: expense.value
         })
-        return this.helper.ok({ data: expenses })
+        return HttpResponseHelper.ok({data : expenses })
     }
 
     async expenses(): Promise<IHttpsResponse> {
         const expenses = await this.expenseDatabase.expenses();
-        return this.helper.ok({ data: expenses })
+        return HttpResponseHelper.ok({data : expenses })
     }
 
     async updateExpense(id: string, expense: IinputNewExpense): Promise<IHttpsResponse> {
@@ -24,16 +22,16 @@ export class ExpenseController implements IExpenseController {
             expense: expense.expense,
             value: expense.value
         })
-        return this.helper.ok({ data: expenses })
+        return HttpResponseHelper.ok({data : expenses })
     }
 
     async deleteExpense(id: string): Promise<IHttpsResponse> {
         const expenses = await this.expenseDatabase.deleteExpense(id);
-        return this.helper.ok({ data: expenses })
+        return HttpResponseHelper.ok({data : expenses })
     }
 
     async updateIsPaid(id: string): Promise<IHttpsResponse> {
         const expenses = await this.expenseDatabase.updateIsPaid(id);
-        return this.helper.ok({ data: expenses })
+        return HttpResponseHelper.ok({data : expenses })
     }
 }
--- backend/src/helpers/helper.ts
+++ backend/src/helpers/helper.ts
@@ -1,11 +1,13 @@
 import { IHelper, IHttpsResponse } from "../interfaces/interfaces";
 
-export class HttpResponseHelper implements IHelper {
-  ok({ statusCode, data, message }: IHttpsResponse): IHttpsResponse {
-    return {
-      statusCode: statusCode ?? 200,
-      message: message ?? "ok",
-      data,
-    };
+export class HttpResponseHelper {
+  private constructor(
+    public readonly statusCode: number, 
+    public readonly message?: string, 
+    public readonly data?: any
+  ) {}
+
+  static ok({ statusCode, message, data }: IHttpsResponse): IHttpsResponse {
+    return new HttpResponseHelper(statusCode ?? 200, message ?? 'Ok', data);
   }
 }
