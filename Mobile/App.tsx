c07d6d3a17cb496417fa4d218f819dfaf5803045
Luan Bianchini <luanbianchine1@gmail.com>
2023-05-03 14:43:54 -0300

    feat: using SWR globally

--- Mobile/App.tsx
+++ Mobile/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Routes from './src/routes/routes';
+import { SWRConfig } from 'swr';
+import axios from 'axios';
 
+// Config global SWR
 export default function App() {
   return (
-    <>
+    <SWRConfig
+      value={{
+        refreshInterval: 3000,
+        fetcher: (url: string) => axios.get(url).then((res) => res.data.data),
+      }}
+    >
       <Routes />
-    </>
+    </SWRConfig>
   );
 }
