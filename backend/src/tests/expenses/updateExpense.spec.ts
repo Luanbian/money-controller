bb882fa4afd1d917efa40fd404b804761ab12967
Luan Bianchini <luanbianchine1@gmail.com>
2023-05-05 13:59:35 -0300

    feat: test for updateExpense

---
+++ backend/src/tests/expenses/updateExpense.spec.ts
@@ -0,0 +1,20 @@
+import { describe, vi, it, expect } from "vitest";
+import { ExpenseController } from "../../controllers/expense.controller";
+
+describe('Update the expense name and value by id', () => {
+    it('should update expense', async () => {
+        const updateExpenseMock = vi.fn().mockResolvedValue([1]);
+        const expenseController = new ExpenseController({updateExpense: updateExpenseMock} as any);
+        const id = '1'
+        const expense = {
+            expense: 'teste',
+            value: 1
+        }
+        await expect(expenseController.updateExpense(id, expense)).resolves.toEqual({
+            statusCode: 200,
+            message: 'Ok',
+            data: [1]
+        })
+        expect(updateExpenseMock).toHaveBeenCalledOnce();
+    })
+})
