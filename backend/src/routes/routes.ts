33e2ec413c5f03f8ed19a0c41ef20d5ddd34e56d
Luan Bianchini <luanbianchine1@gmail.com>
2023-05-05 19:03:41 -0300

    fix: deleting zod in routes

--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,19 +1,10 @@
 import { Router } from "express";
 import { makeGmailController, makeExpenseController } from "../app/factories";
-import { z } from 'zod';
 import { makeMiddleware } from "../middleware/makeMiddleware";
 
 export const router = Router();
 const gmailController = makeGmailController();
 const expenseController = makeExpenseController();
-
-const ExpenseSchema = z.object({
-    expense: z.string(),
-    value: z.number()
-});
-const IdSchema = z.object({
-    id: z.string(),
-});
 
 router.get('/history', makeMiddleware(gmailController.getTransaction));
 router.post('/expense', makeMiddleware(expenseController.newExpense))
